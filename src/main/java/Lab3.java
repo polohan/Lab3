ff92d3950a0a436c72fc3a8bcba5cbd7bbd5c800
17729 <17729@BB>
2017-09-19 14:27:26 -0500

    123

---
+++ src/main/java/Lab3.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+public class Lab3 {
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    }
+
